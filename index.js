35ca470e66b6234f00b9bf95a54c676b5158b60b
Welchj55 <101302492+Welchj55@users.noreply.github.com>
2022-03-31 08:17:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var jelly1=document.getElementById("woodland_stroll")
+
+
+var imageArray = ["./BallastLogonoNetwatermark.png", "./digital corset.png", "./digital logo bk.png"];
+
+// jelly1.src= imageArray[1]
+
+// /--
+// Bootstrap columns and rows for digital, canvas, and sketch pages--/
